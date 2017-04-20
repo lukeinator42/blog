f9c478eaf116e51916d8dd63bfca41e8167ba7c7
lukas <lukas.grasse@uleth.ca>
2017-04-20 17:29:05 -0600

    added disqus

--- components/SitePost/index.jsx
+++ components/SitePost/index.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router';
 import { prefixLink } from 'gatsby-helpers';
 import { config } from 'config';
 import ReadNext from '../ReadNext';
+import ReactDisqusThread from 'react-disqus-comments';
 import './style.css';
 import '../../static/css/highlight.css';
 
@@ -11,6 +12,7 @@ class SitePost extends React.Component {
   render() {
     const { route } = this.props;
     const post = route.page.data;
+    console.log(post);
     const home = (
       <div>
         <Link className="gohome" to={prefixLink('/')}>All Articles</Link>
@@ -27,6 +29,13 @@ class SitePost extends React.Component {
             <div className="date-published">
               <em>Published {moment(post.date).format('D MMM YYYY')}</em>
             </div>
+            <ReactDisqusThread
+            				shortname="lukasgrasse-com"
+            				identifier={post.path}
+            				title={post.title}
+            				url={post.path}
+            				category_id={post.category}
+            				/>
           </div>
           <div className="footer">
             <ReadNext post={post} {...this.props} />
